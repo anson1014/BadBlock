8a4158d8f36cabc5954bda6e88b52b79715affc3
anson1014 <anson1014@hotmail.com>
2019-11-30 16:48:40 -0800

    completed wordReplace function

--- BadBlock.js
+++ BadBlock.js
@@ -1,12 +1,26 @@
-const map = {"bitch": "beep", "fuck": "yo"}
-// fill this map with your python map
-checkWord = function(word){
-   if(map.hasOwnProperty(word)){
-      //Your replace code here
-      console.log(map[word]);
+// const map = {"bitch": "beep", "fuck": "yo"}
+// // fill this map with your python map
+// checkWord = function(word){
+//    if(map.hasOwnProperty(word)){
+//       //Your replace code here
+//       console.log(map[word]);
+//    }
+// }
+// checkWord("bitch");
+// checkWord("yo");
+// checkWord("fuck");
+
+
+wordReplace = function(word){
+   switch (word){
+      default: return "BLEEP"
+      case "fuck": return "diddle"
+      case "fucker": return "diddler"
+      case "fucking": return "diddling"
+      case "motherfucker": return "mother flubber"
+      case "bitch": return "brat"
+      case ("nigger"|"nigga"): return "neighbour"
+      case ("dick"|"penis"): return "male genitalia"
+      case ("pussy"|"vagina"|"cunt"): return "female genitalia"
    }
 }
-checkWord("bitch");
-checkWord("yo");
-checkWord("fuck");
-
