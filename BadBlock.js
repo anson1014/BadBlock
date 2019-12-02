77f74270d2077707654725bad5c721dfc7e59eab
gchu <g0xchu@gmail.com>
2019-12-02 01:33:57 -0800

    working with jquery, adding to the manifest.json file -- fail

--- BadBlock.js
+++ BadBlock.js
@@ -26,13 +26,13 @@ badWordSetAdder = function(word){
 wordReplace = function(word){
    switch (word){
       default: return "BLEEP"
-      case "fuck": return "diddle"
+      case "fuck*": return "diddle"
       case "fucker": return "diddler"
       case "fucking": return "diddling"
       case "motherfucker": return "mother flubber"
       case "bitch": return "brat"
       case ("nigger"|"nigga"): return "neighbour"
-      case ("dick"|"penis"): return "male genitalia"
+      case ("dick*"|"penis"): return "male genitalia"
       case ("pussy"|"vagina"|"cunt"): return "female genitalia"
       }
    }
